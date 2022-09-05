1fe55cd417508afc3b62cbd2e2c03c74185f644e
ashley <97468854+ansomerv@users.noreply.github.com>
2022-09-04 21:39:55 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS KEY to use"
+  default     = "AWS_SECRET_ACCESS_KEY"
+}
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The AWS KEY to use"
+  default     = "AWS_ACCESS_KEY_ID"
+}
