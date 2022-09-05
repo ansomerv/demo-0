a202661c696f24f59384d816669aa602c01621a1
ashley <97468854+ansomerv@users.noreply.github.com>
2022-09-04 22:05:03 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-west-2"
-  AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
-  AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
-}
