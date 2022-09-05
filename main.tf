b2d0058f9780d768be95553d51ff18f11d40d3e0
ashley <97468854+ansomerv@users.noreply.github.com>
2022-09-05 10:49:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example" {
   ami           = "ami-75db401"
   instance_type = "t2.micro"
 
-  #  tags {
-  #    Name = "terraform-example"
-  #  }
+    tags {
+      Name = "terraform-example"
+    }
 }
